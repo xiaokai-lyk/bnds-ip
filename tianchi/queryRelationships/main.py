ab18c9139a7c1a82f5f877490ebb86cb8224790b
xiaokai-lyk <105791771+xiaokai-lyk@users.noreply.github.com>
2024-11-03 16:55:47 +0800

    pytorch template

---
+++ tianchi/queryRelationships/main.py
@@ -0,0 +1,11 @@
+import torch
+import json
+
+def train():
+    pass
+
+def predict():
+    pass
+
+if __name__=='__main__':
+    pass
---
+++ tianchi/queryRelationships/units/model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+net = Net()
+print(net)
